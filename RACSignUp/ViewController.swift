904cb814fab6f09e23bb5e2a31dadc96ce7ccd71
Hirad Motamed <hirad@pendarlabs.com>
2015-10-08 12:47:54 -0700

    Initial Commit

---
+++ RACSignUp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RACSignUp
+//
+//  Created by Hirad Motamed on 2015-10-08.
+//  Copyright © 2015 Pendar Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
